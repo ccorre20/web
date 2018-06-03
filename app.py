c106d392f3e25ba0c8c7c839599b1c7e7d013019
ccorre20 <ccorre20@eafit.edu.co>
2018-06-03 16:40:59 -0500

    t

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, redirect, flash, url_for
+from werkzeug.utils import secure_filename
+import os
+import io
+from processing import classify
+
+
+UPLOAD_FOLDER = '/tmp'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return classify(filename)
+    return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+          <p><input type=file name=file>
+             <input type=submit value=Upload>
+        </form>
+        '''
+
+if __name__ == '__main__':
+    app.run()
---
+++ processing.py
@@ -0,0 +1,37 @@
+import cv2
+import joblib
+import os
+import contextlib
+
+img_folder = '/tmp'
+
+locations = {0: '18', 1: '19', 2: '26', 3: '38', 4: 'admisiones', 5: 'agora', 6: 'auditorio',
+             7: 'biblioteca', 8: 'idiomas', 9: 'dogger'}
+
+def classify(filename):
+    img = cv2.cvtColor(cv2.imread(os.path.join(img_folder, filename)), cv2.COLOR_BGR2GRAY)
+    sift = cv2.xfeatures2d.SIFT_create()
+    _, des = sift.detectAndCompute(img, None)
+
+    k_model = joblib.load('model_500.pkl')
+
+    labels = k_model.predict(des)
+
+    hist = {}
+
+    for i in range(500):
+        hist[i] = 0
+
+    for l in labels:
+        hist[l] = hist[l] + 1
+
+    sample = list(hist.values())
+
+    clf = joblib.load('LinearSVC_500.pkl')
+
+    ans = locations[clf.predict([sample])[0]]
+
+    with contextlib.suppress(FileNotFoundError):
+        os.remove(os.path.join(img_folder, filename))
+
+    return ans
