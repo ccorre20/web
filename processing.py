adc9ceea34e6d20892640612d14b6804e1bb22c6
ccorre20 <ccorre20@eafit.edu.co>
2018-06-05 10:15:38 -0500

    new model

--- processing.py
+++ processing.py
@@ -2,34 +2,38 @@ import cv2 as cv2
 import joblib
 import os
 import contextlib
+import pandas as pd
+import numpy as np
 
 img_folder = '/tmp'
 
 locations = {0: '18', 1: '19', 2: '26', 3: '38', 4: 'admisiones', 5: 'agora', 6: 'auditorio',
              7: 'biblioteca', 8: 'idiomas', 9: 'dogger'}
 
+k = 500
+
 def classify(filename):
     img = cv2.cvtColor(cv2.imread(os.path.join(img_folder, filename)), cv2.COLOR_BGR2GRAY)
+
+    h, w, c = img.shape
+
+    if w > 2000:
+        img = cv2.resize(img, (1280, 720))
+
     sift = cv2.xfeatures2d.SIFT_create()
     _, des = sift.detectAndCompute(img, None)
 
-    k_model = joblib.load('model_500.pkl')
+    df = pd.DataFrame(des)
 
-    labels = k_model.predict(des)
+    k_model = joblib.load('new_KMeans.pkl')
 
-    hist = {}
+    labels = k_model.predict(df.values)
 
-    for i in range(500):
-        hist[i] = 0
+    hist, _ = np.histogram(labels, k, density=True)
 
-    for l in labels:
-        hist[l] = hist[l] + 1
+    clf = joblib.load('new_LinearSVC.pkl')
 
-    sample = list(hist.values())
-
-    clf = joblib.load('LinearSVC_500.pkl')
-
-    ans = locations[clf.predict([sample])[0]]
+    ans = locations[clf.predict([hist])]
 
     with contextlib.suppress(FileNotFoundError):
         os.remove(os.path.join(img_folder, filename))
